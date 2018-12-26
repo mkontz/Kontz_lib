a24a5a2a7f6cf428dfcb297959438997999d48f8
Matt Kontz <mkontz@gmail.com>
2018-12-25 20:55:58 -0600

    Added class to find next prime number after a give number.  Includes class and tests.

---
+++ PrimeNumber/prime.h
@@ -0,0 +1,153 @@
+#ifndef _PRIME_H__
+#define _PRIME_H__
+
+#include <vector>
+#include <algorithm>
+
+namespace MEK
+{
+	template<typename T> class Primes
+	{
+	private:
+		std::vector<T> m_primes;
+
+	public:
+
+		Primes()
+		{
+			m_primes.push_back(2);
+			m_primes.push_back(3);
+			m_primes.push_back(5);
+			m_primes.push_back(7);
+		}
+
+		Primes(T max_value)
+		{
+			m_primes.push_back(2);
+			m_primes.push_back(3);
+			m_primes.push_back(5);
+			m_primes.push_back(7);
+
+			addPrimesLessThan(max_value);
+		}
+
+		~Primes() { }
+
+		Primes& operator = (const Primes& other)
+		{
+			return m_primes = other.getPrimes();
+		}
+
+		void setPrimes(std::vector<T> primes) { m_primes = primes; }
+		std::vector<T> getPrimes() const { return m_primes; }
+
+		T findNextPrime()
+		{
+			return findNextPrime(m_primes.back());
+		}
+
+		T findNextPrime(T number)
+		{
+			if (number < m_primes.back())
+			{
+				// If prime number is already be in cashed list....find prime number in list
+				//    use binary search
+				size_t k1 = 0;
+				size_t k2 = m_primes.size() - 1;
+				size_t k_mid;
+
+				while ((k1 + 1) <  k2)
+				{
+					k_mid = (k1 + k2) / 2;
+
+					if (number < m_primes.at(k_mid))
+					{
+						k2 = k_mid;
+					}
+					else
+					{
+						k1 = k_mid;
+					}
+				}
+
+				number = m_primes.at(k2);
+			}
+			else // next prime number is not is cached list
+			{
+				bool notPrime = true;  // assume each loop it is a prime number
+
+				// There are some special case related to 2, 3 & 5, but these will be handled by number in the initial prime list
+				// From this point on it can be assume that the last digit in all primes are 1, 3, 7 or 9.
+
+				// Even numbers can't be prime...except 2
+				if ((number % 2) == 0)
+				{
+					number--;
+				}
+
+				// initialize moduous variables and declare variables outside of loops
+				uint8_t mod3 = (number % 3);
+				uint8_t mod10 = (number % 10);
+				uint8_t delta;
+				size_t k_sqrt = 1;
+
+				while (notPrime)
+				{
+					// assume the number is a prime and prove otherwise
+					notPrime = false;
+
+					delta = (mod10 == 3) ? 4 : 2; // skip odd numbers ending in 5
+					number += delta;
+					mod3 = (mod3 + delta) % 3;
+					mod10 = (mod10 + delta) % 10;
+
+					// check 3
+					if (mod3 == 0)
+					{
+						notPrime = true;
+					}
+					else
+					{
+						// Ensure that list of prime are at least as big as the the square root of the number
+						while ((m_primes.at(k_sqrt) * m_primes.at(k_sqrt)) < number)
+						{
+							k_sqrt++;
+
+							if (m_primes.size() <= k_sqrt)
+							{
+								addNextPrime();
+							}
+						}
+
+						for (size_t k = 2; k <= k_sqrt; k++)
+						{
+							if ((number % m_primes.at(k)) == 0)
+							{
+								notPrime = true;
+								break;
+							}
+						}
+					}
+				}
+			}
+
+			return number;
+		}
+
+		void addNextPrime()
+		{
+			m_primes.push_back(findNextPrime());
+		}
+
+		void addPrimesLessThan(T max_value)
+		{
+			while (m_primes.back() < max_value)
+			{
+				addNextPrime();
+			}
+			m_primes.pop_back();
+		}
+	};
+}
+
+#endif
---
+++ PrimeNumber/test/test.cpp
@@ -0,0 +1,324 @@
+#include <iostream>
+#include <string>
+#include <ctime>
+#include "../prime.h"
+
+#define private public
+#define protected public
+
+using namespace MEK;
+// using namespace std;
+
+template<typename T>
+bool checkNextPrime(Primes<T> primeTest, T input, T expected, bool passed = true)
+{
+	T output = primeTest.findNextPrime(input);
+
+	if (expected == output)
+	{
+		std::cout << "    Next prime after " << input << " is " << output << " - Passed\n";
+	}
+	else
+	{
+		passed = false;
+		std::cout << "    Next prime after " << input << " is NOT " << output << " - Failed\n";
+	}
+
+	return passed;
+}
+
+int main()
+{
+	bool passed = true;
+	bool allTestsPassed = true;
+
+	{
+		std::cout << "Testing Primes class #1.\n";
+		passed = true;
+
+		unsigned max_value = 100;
+
+		std::clock_t begin = clock();
+		Primes<unsigned> primeTest(max_value);
+		std::clock_t end = clock();
+
+		double primeTime = double(end - begin) / CLOCKS_PER_SEC;
+
+		begin = clock();
+		std::vector<unsigned> bruteForce;
+		for (unsigned k = 2; k <= max_value; k++)
+		{
+			bool isPrime = true;
+			for (unsigned h = 2; h <k; h++)
+			{
+				if ((k % h) == 0)
+				{
+					isPrime = false;
+					break;
+				}
+			}
+
+			if (isPrime)
+			{
+				bruteForce.push_back(k);
+			}
+		}
+		end = clock();
+
+		double bruteTime = double(end - begin) / CLOCKS_PER_SEC;
+
+		std::vector<unsigned> prime_list = primeTest.getPrimes();
+		std::cout << "    list of prime = ["  << prime_list.at(0);
+		for (size_t k = 1; k < 15; k++) // prime_list.size(); k++)
+		{
+			std::cout << " " << prime_list.at(k);
+		}
+		std::cout << "]\n";
+
+		passed = true;
+		if (bruteForce.size() != prime_list.size())
+		{
+			std::cout << "    Length of prime lists don't match." << std::endl;
+			passed = false;
+		}
+		else
+		{
+			for (size_t k = 0; k < bruteForce.size(); k++)
+			{
+				if (bruteForce.at(k) != prime_list.at(k))
+				{
+					std::cout << "    lists don't match: " << bruteForce.at(k) << ", " << bruteForce.at(k) << std::endl;
+					passed = false;
+				}
+			}
+		}
+
+		std::cout << "    prime time = " << primeTime << ", brute force time = " << bruteTime << std::endl << std::endl;
+
+		allTestsPassed = (allTestsPassed && passed);
+	}
+
+	{
+		std::cout << "Testing Primes class #2.\n";
+
+		passed = true;
+
+		int max_value = 1234567;
+
+		std::clock_t begin = clock();
+		Primes<uint32_t> primeTest(max_value);
+		primeTest.addNextPrime();
+		std::clock_t end = clock();
+
+		double primeListTime = double(end - begin) / CLOCKS_PER_SEC;
+
+		begin = clock();
+		Primes<uint32_t> primeTest2;
+		uint32_t nextPrime = primeTest2.findNextPrime(max_value);
+		end = clock();
+
+		double nextPrimeTime = double(end - begin) / CLOCKS_PER_SEC;
+
+		if (primeTest.getPrimes().back() != nextPrime)
+		{
+			passed = false;
+			std::cout << "    last prime in list: " << primeTest.getPrimes().back() << ", next prime method: " << nextPrime << std::endl;
+		}
+		else
+		{
+			std::cout << "    next prime method: " << nextPrime << std::endl;
+		}
+
+		std::cout << "    prime list  time = " << primeListTime << ", next prime time = " << nextPrimeTime << std::endl << std::endl;
+
+		allTestsPassed = (allTestsPassed && passed);
+	}
+
+	{
+		std::cout << "Testing Primes class with primes in list.\n";
+
+		passed = true;
+
+		Primes<uint16_t> primeTest(20);
+
+		uint16_t input = 2;
+		uint16_t expected = 3;
+		passed = checkNextPrime(primeTest, input, expected, passed);
+
+		input = 3;
+		expected = 5;
+		passed = checkNextPrime(primeTest, input, expected, passed);
+
+		input = 4;
+		expected = 5;
+		passed = checkNextPrime(primeTest, input, expected, passed);
+
+		input = 5;
+		expected = 7;
+		passed = checkNextPrime(primeTest, input, expected, passed);
+
+		input = 6;
+		expected = 7;
+		passed = checkNextPrime(primeTest, input, expected, passed);
+
+		input = 7;
+		expected = 11;
+		passed = checkNextPrime(primeTest, input, expected, passed);
+
+		input = 8;
+		expected = 11;
+		passed = checkNextPrime(primeTest, input, expected, passed);
+
+		input = 9;
+		expected = 11;
+		passed = checkNextPrime(primeTest, input, expected, passed);
+
+		input = 10;
+		expected = 11;
+		passed = checkNextPrime(primeTest, input, expected, passed);
+
+		input = 11;
+		expected = 13;
+		passed = checkNextPrime(primeTest, input, expected, passed);
+
+		input = 12;
+		expected = 13;
+		passed = checkNextPrime(primeTest, input, expected, passed);
+
+		input = 13;
+		expected = 17;
+		passed = checkNextPrime(primeTest, input, expected, passed);
+
+		input = 14;
+		expected = 17;
+		passed = checkNextPrime(primeTest, input, expected, passed);
+
+		input = 15;
+		expected = 17;
+		passed = checkNextPrime(primeTest, input, expected, passed);
+
+		input = 16;
+		expected = 17;
+		passed = checkNextPrime(primeTest, input, expected, passed);
+
+		input = 17;
+		expected = 19;
+		passed = checkNextPrime(primeTest, input, expected, passed);
+
+		input = 18;
+		expected = 19;
+		passed = checkNextPrime(primeTest, input, expected, passed);
+
+		input = 19;
+		expected = 23;
+		passed = checkNextPrime(primeTest, input, expected, passed);
+
+		std::cout << std::endl;
+
+		allTestsPassed = (allTestsPassed && passed);
+	}
+
+	{
+		std::cout << "Testing Primes class without primes in list.\n";
+
+		passed = true;
+
+		Primes<uint16_t> primeTest;
+
+		uint16_t input = 2;
+		uint16_t expected = 3;
+		passed = checkNextPrime(primeTest, input, expected, passed);
+
+		input = 3;
+		expected = 5;
+		passed = checkNextPrime(primeTest, input, expected, passed);
+
+		input = 4;
+		expected = 5;
+		passed = checkNextPrime(primeTest, input, expected, passed);
+
+		input = 5;
+		expected = 7;
+		passed = checkNextPrime(primeTest, input, expected, passed);
+
+		input = 6;
+		expected = 7;
+		passed = checkNextPrime(primeTest, input, expected, passed);
+
+		input = 7;
+		expected = 11;
+		passed = checkNextPrime(primeTest, input, expected, passed);
+
+		input = 8;
+		expected = 11;
+		passed = checkNextPrime(primeTest, input, expected, passed);
+
+		input = 9;
+		expected = 11;
+		passed = checkNextPrime(primeTest, input, expected, passed);
+
+		input = 10;
+		expected = 11;
+		passed = checkNextPrime(primeTest, input, expected, passed);
+
+		input = 11;
+		expected = 13;
+		passed = checkNextPrime(primeTest, input, expected, passed);
+
+		input = 12;
+		expected = 13;
+		passed = checkNextPrime(primeTest, input, expected, passed);
+
+		input = 13;
+		expected = 17;
+		passed = checkNextPrime(primeTest, input, expected, passed);
+
+		input = 14;
+		expected = 17;
+		passed = checkNextPrime(primeTest, input, expected, passed);
+
+		input = 15;
+		expected = 17;
+		passed = checkNextPrime(primeTest, input, expected, passed);
+
+		input = 16;
+		expected = 17;
+		passed = checkNextPrime(primeTest, input, expected, passed);
+
+		input = 17;
+		expected = 19;
+		passed = checkNextPrime(primeTest, input, expected, passed);
+
+		input = 18;
+		expected = 19;
+		passed = checkNextPrime(primeTest, input, expected, passed);
+
+		input = 19;
+		expected = 23;
+		passed = checkNextPrime(primeTest, input, expected, passed);
+
+		std::cout << std::endl;
+
+		allTestsPassed = (allTestsPassed && passed);
+	}
+
+
+	////////////////////////////////////////////////////////////////
+	// Summary of all tests.
+	////////////////////////////////////////////////////////////////
+	if (allTestsPassed)
+	{
+		std::cout << "All tests passed!!\n\n";
+	}
+	else
+	{
+		std::cout << "Not all tests passed!!\n\n";
+	}
+	////////////////////////////////////////////////////////////////
+
+	return 0;
+}
+
+
+
+
